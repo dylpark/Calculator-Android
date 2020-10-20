7bcecd2aa3abec80f88dfcd39244c78ace325b8d
dparksy <dylanpark.net@gmail.com>
2020-10-20 15:51:59 +1000

    Update MainActivity.kt

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun onDigit(view: View){
+        Toast.makeText(this, "Button works", Toast.LENGTH_SHORT ).show()
     }
 }
