767f79ef41ab74bf03c4f6c69cb89a6c79213b12
dparksy <dylanpark.net@gmail.com>
2020-10-20 16:29:06 +1000

    Update MainActivity.kt

--- app/src/main/java/com/example/calculator/MainActivity.kt
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -3,16 +3,35 @@ package com.example.calculator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var lastNumeric : Boolean = false
+    var lastDot : Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 
     fun onDigit(view: View){
-        tvInput.append("1")
+        tvInput.append((view as Button).text)
+        lastNumeric = true
     }
+
+    fun onClear(view: View) {
+        tvInput.text = ""
+    }
+
+    fun onDecimal(view: View){
+        if(lastNumeric && !lastDot){
+            tvInput.append(".")
+            lastNumeric = false
+            lastDot = true
+        }
+    }
+
 }
